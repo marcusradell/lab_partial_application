19162f84f217ed719e37f2defdbfc5482794fbcf
Marcus Rådell <marcus@radell.net>
2023-03-06 14:08:42 +0100

    add two more labs and reorder them

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,21 @@
-test("Lab 1", async () => {
+test("Lab 0: Basics", async () => {
+  const result = getItem();
+
+  expect(result).toEqual({ name: "Shovel" });
+});
+
+test("Lab 1: Factory function", () => {
   const getAllItems = createGetAllItems();
 
-  const result = await getAllItems();
+  const result = getAllItems();
+
+  expect(result).toEqual([{ name: "Shovel" }]);
+});
+
+test("Lab 2: Async", async () => {
+  const fetchAllItems = createFetchAllItems();
+
+  const result = await fetchAllItems();
 
   expect(result).toEqual([{ name: "Shovel" }]);
 });
